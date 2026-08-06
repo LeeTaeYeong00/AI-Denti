6ec9e1279c8853ffd90604e3bb0f78d3e3cd204d
LeeTaeYeong00 <sapsaldog10@naver.com>
2026-08-06 12:44:17 +0900

    chore: 프로젝트 초기 설정

--- denti_back/src/main/java/com/example/denti_back/DentiBackApplication.java
+++ denti_back/src/main/java/com/example/denti_back/DentiBackApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-// DB 자동 설정을 일시적으로 제외 (DataSourceAutoConfiguration.class)
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = (DataSourceAutoConfiguration.class))
 public class DentiBackApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DentiBackApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DentiBackApplication.class, args);
+	}
 
 }
